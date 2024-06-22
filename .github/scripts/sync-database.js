1c968a9a4c7869f861fb79e5426141fcd7c614f2
Ry0taK <49341894+Ry0taK@users.noreply.github.com>
2024-06-22 16:27:55 +0900

    続々と変更される継続的インテグレーション

---
+++ .github/scripts/sync-database.js
@@ -0,0 +1,72 @@
+
+const fs = require('fs');
+const cp = require('child_process');
+const github = require('@actions/github');
+
+function configureGit() {
+    cp.execSync('git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"');
+    cp.execSync('git config --global user.name "github-actions[bot]"');
+}
+
+function checkoutBranch(branch) {
+    try {
+        cp.execFileSync('git', ['checkout', branch]);
+    } catch (e) {
+        cp.execFileSync('git', ['checkout', '-b', branch]);
+    }
+    cp.execSync('git pull');
+}
+
+function sync() {
+    cp.execSync('git add .');
+    cp.execSync('git commit -m "Sync database"', { cwd: 'database' });
+    cp.execSync('git push --all', { cwd: 'database' });
+}
+const owner = process.env.GITHUB_REPOSITORY_OWNER;
+const repo = process.env.GITHUB_REPOSITORY.split('/')[1];
+const issue = JSON.parse(process.env.ISSUE_PAYLOAD);
+const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
+
+try {
+    configureGit();
+    checkoutBranch('database');
+    const id = `CRE-${new Date().getFullYear()}-${36000 + fs.readdirSync('database').length}`;
+    const body = JSON.parse(issue.body);
+    if (!body.author || !body.text || !body.vector_string) {
+        throw new Error('Invalid issue body');
+    }
+    body.id = id;
+    fs.writeFileSync(`${id}.json`, JSON.stringify(body, null, 2));
+    console.log(`Wrote issue to ${id}.json`);
+    const files = fs.readdirSync('.').filter(file => file.endsWith('.json'));
+    const all = files.map(file => JSON.parse(fs.readFileSync(file)));
+    checkoutBranch('generated');
+    fs.writeFileSync('all.json', JSON.stringify(all, null, 2));
+    //sync();
+    octokit.rest.issues.createComment({
+        issue_number: issue.number,
+        owner: owner,
+        repo: repo,
+        body: `Registered oyajigyagu with ID [${id}](https://crss.gnknzm.net/details/?id=${id})`
+    });
+    octokit.rest.issues.update({
+        issue_number: issue.number,
+        owner: owner,
+        repo: repo,
+        state: 'closed'
+    });
+} catch (e) {
+    octokit.rest.issues.createComment({
+        issue_number: issue.number,
+        owner: owner,
+        repo: repo,
+        body: `Closing issue due to the invalid body.`
+    });
+    octokit.rest.issues.update({
+        issue_number: issue.number,
+        owner: owner,
+        repo: repo,
+        state: 'closed'
+    });
+    console.log(e);
+}
